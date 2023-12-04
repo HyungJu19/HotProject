0d642bc87301d9948cb341a43c612eb8353494dd
기원 <gkgk7610@naver.com>
2023-12-04 18:04:36 +0900

    #기원 foodieController 수정  - 주소 수정 foodieMain.html 수정  - 자바스크립트 경로 수정

--- src/main/java/com/lec/spring/controller/FoodieController.java
+++ src/main/java/com/lec/spring/controller/FoodieController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/theme/foodie")
 public class FoodieController {
-    @GetMapping("/main")
-    public void main(){}
+    @GetMapping("/foodieMain")
+    public void foodieMain(){}
 }
