4669c84d677f33759d035f6301e278d5739778b7
minho <hyungju911@gmail.com>
2023-12-06 21:29:05 +0900

    #민호 메인 지도 ui 제작 어드민 데이터최신화 완료

--- src/main/resources/static/JS/mainmap.js
+++ src/main/resources/static/JS/mainmap.js
@@ -1,4 +1,9 @@
 function moveImage(area) {
+    const textContent = document.querySelector(`#${area}TextDiv`);
+    textContent.style.backgroundColor = '#000'; // 배경색을 검정색으로 변경
+    textContent.style.color = '#fff'; // 텍스트 색상을 흰색으로 변경
+    const textContent1 = document.querySelector(` #${area}TextDiv .areaText`);
+    textContent1.style.color = '#fff'; // 텍스트 색상을 흰색으로 변경
     const image = document.getElementById(`${area}Image`);
     let scaleValue = 'scale(1.0)'; // 기본 스케일 값
 
@@ -10,8 +15,16 @@ function moveImage(area) {
     }
 
     image.style.transform = scaleValue;
+
+
+
 }
 function resetImage(area) {
+    const textContent = document.querySelector(`#${area}TextDiv `);
+    textContent.style.backgroundColor = '#fff'; // 배경색을 원래대로 흰색으로 변경
+    textContent.style.color = '#000'; // 텍스트 색상을 원래대로 검정색으로 변경
+    const textContent1 = document.querySelector(`#${area}TextDiv .areaText`);
+    textContent1.style.color = '#000'; // 텍스트 색상을 원래대로 검정색으로 변경
     const image = document.getElementById(`${area}Image`);
-    image.style.transform = 'scale(1.0)';
+    image.style.transform = 'scale(1.0)'; // 이미지를 원래 크기로 되돌림
 }
