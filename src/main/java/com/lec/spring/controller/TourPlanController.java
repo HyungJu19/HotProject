d20854ba560df43c201d56db0c594d4f0647a8cd
hyungju19 <hyungju911@gmail.com>
2023-12-26 02:48:22 +0900

    #민호  스케줄 80% 완성 수정중

--- src/main/java/com/lec/spring/controller/TourPlanController.java
+++ src/main/java/com/lec/spring/controller/TourPlanController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.Objects;
 
 @Controller
-@RequestMapping("/schedule/tour")
+@RequestMapping("/schedule")
 public class TourPlanController {
 
     @Autowired
@@ -23,7 +23,7 @@ public class TourPlanController {
     @Autowired
     private KakaoMapProperties kakaoMapProperties;
 
-    @GetMapping
+    @GetMapping("/tour")
     public String search(HttpServletRequest request, Model model) {
         String area = request.getParameter("area");
         String areaCode = request.getParameter("areaCode");
@@ -33,21 +33,83 @@ public class TourPlanController {
         String orderby = request.getParameter("oderby");
         int limit = 20;
         int offset = (page - 1) * limit;
-        List<TouristData> dataList = touristService.touristDataList(area, areaCode, contentTypeId, limit, offset);
+        List<TouristData> dataList = touristService.touristDataList1(area, areaCode, contentTypeId,orderby, limit, offset);
         model.addAttribute("dataList", dataList);
 
         List<CampingData> campingDataList = touristService.campingDataList(area, areaCode, orderby, limit, offset);
         model.addAttribute("campingDataList", campingDataList);
 
+        int totalItems = touristService.getTotalDataCount(areaCode, contentTypeId);
+        int totalPages = (int) Math.ceil((double) totalItems / limit);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("areaCode", areaCode);
+        model.addAttribute("contentTypeId", contentTypeId);
+
 
         String kakaoMapApiKey = kakaoMapProperties.getApikey();
         model.addAttribute("kakaoMapApiKey", kakaoMapApiKey);
-
-
+        int totalCount = touristService.getTotalAreacodeCount(areaCode, contentTypeId);
+        model.addAttribute("totalCount", totalCount);
+        int campingTotalCount = touristService.getConpingAreaTotalCount(area);
+        model.addAttribute("campintTotalCount", campingTotalCount);
         System.out.println(dataList + "????");
+
+        String buttonText = "";
+
+        if (contentTypeId == null) {
+            buttonText = "캠핑";
+        } else {
+            if (contentTypeId.equals("12")) {
+                buttonText = "관광지";
+            } else if (contentTypeId.equals("39")) {
+                buttonText = "음식점";
+            } else if (contentTypeId.equals("32")) {
+                buttonText = "숙박";
+            } else if (contentTypeId.equals("38")) {
+                buttonText = "쇼핑";
+            } else if (contentTypeId.equals("28")) {
+                buttonText = "레포츠";
+            } else if (contentTypeId.equals("15")) {
+                buttonText = "축제";
+            } else if (contentTypeId.equals("14")) {
+                buttonText = "문화시설";
+            }
+        }
+        model.addAttribute("buttonText", buttonText);
 
         return "schedule/tour";
 
+    }
+
+
+    @GetMapping("save")
+    public String toursave (){
+
+        return null;
+
+    }
+    @GetMapping("save")
+    public String toursaveOk (){
+
+        return null;
+
+    }
+
+    @GetMapping("delete")
+    public String deleteOk(){
+
+        return null;
+    }
+
+
+    @GetMapping("tourList")
+    public String tourList (HttpServletRequest request,Model model){
+
+
+
+
+        return "schedule/tourList";
     }
 
 
